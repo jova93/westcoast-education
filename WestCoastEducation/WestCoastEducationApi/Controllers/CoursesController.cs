e9933bae500797a66cfbbdd46cbcf0c6a5103a97
jova93 <89088512+jova93@users.noreply.github.com>
2022-03-31 11:36:06 +0200

    sixth commit

--- WestCoastEducation/WestCoastEducationApi/Controllers/CoursesController.cs
+++ WestCoastEducation/WestCoastEducationApi/Controllers/CoursesController.cs
@@ -16,67 +16,144 @@ public class CoursesController : ControllerBase
         _coursesService = coursesService;
     }
 
+    //[HttpGet]
+    //public async Task<List<Course>> Get() => await _coursesService.GetAsync();
+
     [HttpGet]
-    public async Task<List<Course>> Get() => await _coursesService.GetAsync();
+    public async Task<IActionResult> Get()
+    {
+        var result = await _coursesService.GetAsync();
 
-    //USE THIS LATER!!
-    //public async Task<IActionResult> Get()
+        if (result is null)
+        {
+            return NotFound();
+        }
+
+        var courses = new List<CourseViewModel>();
+
+        foreach (var course in result)
+        {
+            var temp = new CourseViewModel
+            {
+                Id = course.Id,
+                Number = course.Number,
+                Title = course.Title,
+                Description = course.Description,
+                Duration = course.Duration,
+                Level = course.Level,
+                IsActive = course.IsActive,
+                Price = course.Price,
+                Participants = course.Participants
+            };
+
+            courses.Add(temp);
+        }
+
+        return Ok(courses);
+    }
+
+    //[HttpGet("{id:length(24)}")]
+    //public async Task<ActionResult<Course>> Get(string id)
     //{
-    //    var result = await _coursesService.GetAsync();
+    //    var course = await _coursesService.GetAsync(id);
 
-    //    var courses = new List<CourseViewModel>();
-
-    //    if (result is null)
+    //    if (course is null)
     //    {
     //        return NotFound();
     //    }
 
-    //    foreach (var course in result)
-    //    {
-    //        var temp = new CourseViewModel
-    //        {
-    //            Id = course.Id,
-    //            Number = course.Number,
-    //            Title = course.Title,
-    //            Description = course.Description,
-    //            Duration = course.Duration,
-    //            Level = course.Level,
-    //            IsActive = course.IsActive,
-    //            Price = course.Price,
-    //            Participants = course.Participants
-    //        };
-
-    //        courses.Add(temp);
-    //    }
-
-    //    return Ok(courses);
+    //    return course;
     //}
 
     [HttpGet("{id:length(24)}")]
-    public async Task<ActionResult<Course>> Get(string id)
+    public async Task<IActionResult> Get(string id)
     {
-        var course = await _coursesService.GetAsync(id);
-
-        if (course is null)
+        try
         {
-            return NotFound();
-        }
+            var course = await _coursesService.GetAsync(id);
 
-        return course;
+            if (course is null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new CourseViewModel
+            {
+                Id = course.Id,
+                Number = course.Number,
+                Title = course.Title,
+                Description = course.Description,
+                Duration = course.Duration,
+                Level = course.Level,
+                IsActive = course.IsActive,
+                Price = course.Price,
+                Participants = course.Participants
+            };
+
+            return Ok(viewModel);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
+
+
+    //[HttpPost]
+    //public async Task<IActionResult> Post(Course newCourse)
+    //{
+    //    await _coursesService.CreateAsync(newCourse);
+
+    //    return CreatedAtAction(nameof(Get), new { id = newCourse.Id }, newCourse);
+    //}
 
     [HttpPost]
-    public async Task<IActionResult> Post(Course newCourse)
+    public async Task<IActionResult> Post(AddCourseViewModel viewModel)
     {
-        await _coursesService.CreateAsync(newCourse);
+        try
+        {
+            var course = new Course
+            {
+                Id = viewModel.Id,
+                Number = viewModel.Number,
+                Title = viewModel.Title,
+                Description = viewModel.Description,
+                Duration = viewModel.Duration,
+                Level = viewModel.Level,
+                IsActive = viewModel.IsActive,
+                Price = viewModel.Price,
+                Participants = viewModel.Participants
+            };
 
-        return CreatedAtAction(nameof(Get), new { id = newCourse.Id }, newCourse);
+            await _coursesService.CreateAsync(course);
+
+            return StatusCode(201);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
 
-    //create public async Task<IActionResult> AddCourse(AddCourseViewModel model)
+    //[HttpPut("{id:length(24)}")]
+    //public async Task<IActionResult> Update(string id, Course updatedCourse)
+    //{
+    //    var course = await _coursesService.GetAsync(id);
+
+    //    if (course is null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    updatedCourse.Id = course.Id;
+
+    //    await _coursesService.UpdateAsync(id, updatedCourse);
+
+    //    return NoContent();
+    //}
 
     [HttpPut("{id:length(24)}")]
-    public async Task<IActionResult> Update(string id, Course updatedCourse)
+    public async Task<IActionResult> Update(string id, UpdateCourseViewModel viewModel)
     {
         var course = await _coursesService.GetAsync(id);
 
@@ -85,25 +162,54 @@ public class CoursesController : ControllerBase
             return NotFound();
         }
 
-        updatedCourse.Id = course.Id;
+        course.Number = viewModel.Number;
+        course.Title = viewModel.Title;
+        course.Description = viewModel.Description;
+        course.Duration = viewModel.Duration;
+        course.Level = viewModel.Level;
+        course.IsActive = viewModel.IsActive;
+        course.Price = viewModel.Price;
+        course.Participants = viewModel.Participants;
 
-        await _coursesService.UpdateAsync(id, updatedCourse);
+        await _coursesService.UpdateAsync(id, course);
 
         return NoContent();
     }
+
+    //[HttpDelete("{id:length(24)}")]
+    //public async Task<IActionResult> Delete(string id)
+    //{
+    //    var course = await _coursesService.GetAsync(id);
+
+    //    if (course is null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    await _coursesService.RemoveAsync(id);
+
+    //    return NoContent();
+    //}
 
     [HttpDelete("{id:length(24)}")]
     public async Task<IActionResult> Delete(string id)
     {
-        var course = await _coursesService.GetAsync(id);
-
-        if (course is null)
+        try
         {
-            return NotFound();
+            var course = await _coursesService.GetAsync(id);
+
+            if (course is null)
+            {
+                return NotFound();
+            }
+
+            await _coursesService.RemoveAsync(id);
+
+            return NoContent();
         }
-
-        await _coursesService.RemoveAsync(id);
-
-        return NoContent();
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
 }
--- WestCoastEducation/WestCoastEducationApi/Controllers/StudentsController.cs
+++ WestCoastEducation/WestCoastEducationApi/Controllers/StudentsController.cs
@@ -1,6 +1,7 @@
 ﻿using WestCoastEducationApi.Models;
 using WestCoastEducationApi.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using WestCoastEducationApi.ViewModels;
 
 namespace WestCoastEducationApi.Controllers;
 
@@ -15,32 +16,140 @@ public class StudentsController : ControllerBase
         _studentsService = studentsService;
     }
 
+    //[HttpGet]
+    //public async Task<List<Student>> Get() => await _studentsService.GetAsync();
+
     [HttpGet]
-    public async Task<List<Student>> Get() => await _studentsService.GetAsync();
+    public async Task<IActionResult> Get()
+    {
+        var result = await _studentsService.GetAsync();
+
+        if (result is null)
+        {
+            return NotFound();
+        }
+
+        var students = new List<StudentViewModel>();
+
+        foreach (var student in result)
+        {
+            var temp = new StudentViewModel
+            {
+                Id = student.Id,
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                Email = student.Email,
+                PhoneNumber = student.PhoneNumber,
+                Address = student.Address,
+                TotalSpent = student.TotalSpent,
+                PurchasedCourses = student.PurchasedCourses
+            };
+
+            students.Add(temp);
+        }
+
+        return Ok(students);
+    }
+
+    //[HttpGet("{id:length(24)}")]
+    //public async Task<ActionResult<Student>> Get(string id)
+    //{
+    //    var student = await _studentsService.GetAsync(id);
+
+    //    if (student is null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    return student;
+    //}
 
     [HttpGet("{id:length(24)}")]
-    public async Task<ActionResult<Student>> Get(string id)
+    public async Task<IActionResult> Get(string id)
     {
-        var student = await _studentsService.GetAsync(id);
-
-        if (student is null)
+        try
         {
-            return NotFound();
-        }
+            var student = await _studentsService.GetAsync(id);
 
-        return student;
+            if (student is null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new StudentViewModel
+            {
+                Id = student.Id,
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                Email = student.Email,
+                PhoneNumber = student.PhoneNumber,
+                Address = student.Address,
+                TotalSpent = student.TotalSpent,
+                PurchasedCourses = student.PurchasedCourses
+            };
+
+            return Ok(viewModel);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
+
+    //[HttpPost]
+    //public async Task<IActionResult> Post(Student newStudent)
+    //{
+    //    await _studentsService.CreateAsync(newStudent);
+
+    //    return CreatedAtAction(nameof(Get), new { id = newStudent.Id }, newStudent);
+    //}
 
     [HttpPost]
-    public async Task<IActionResult> Post(Student newStudent)
+    public async Task<IActionResult> Post(AddStudentViewModel viewModel)
     {
-        await _studentsService.CreateAsync(newStudent);
+        try
+        {
+            var student = new Student
+            {
+                Id = viewModel.Id,
+                FirstName = viewModel.FirstName,
+                LastName = viewModel.LastName,
+                Email = viewModel.Email,
+                PhoneNumber = viewModel.PhoneNumber,
+                Address = viewModel.Address,
+                TotalSpent = viewModel.TotalSpent,
+                PurchasedCourses = viewModel.PurchasedCourses
+            };
 
-        return CreatedAtAction(nameof(Get), new { id = newStudent.Id }, newStudent);
+            await _studentsService.CreateAsync(student);
+
+            return StatusCode(201);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
 
+    //[HttpPut("{id:length(24)}")]
+    //public async Task<IActionResult> Update(string id, Student updatedStudent)
+    //{
+    //    var student = await _studentsService.GetAsync(id);
+
+    //    if (student is null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    updatedStudent.Id = student.Id;
+
+    //    await _studentsService.UpdateAsync(id, updatedStudent);
+
+    //    return NoContent();
+    //}
+
     [HttpPut("{id:length(24)}")]
-    public async Task<IActionResult> Update(string id, Student updatedStudent)
+    public async Task<IActionResult> Put(string id, UpdateStudentViewModel viewModel)
     {
         var student = await _studentsService.GetAsync(id);
 
@@ -49,25 +158,53 @@ public class StudentsController : ControllerBase
             return NotFound();
         }
 
-        updatedStudent.Id = student.Id;
+        student.FirstName = viewModel.FirstName;
+        student.LastName = viewModel.LastName;
+        student.Email = viewModel.Email;
+        student.PhoneNumber = viewModel.PhoneNumber;
+        student.Address = viewModel.Address;
+        student.TotalSpent = viewModel.TotalSpent;
+        student.PurchasedCourses = viewModel.PurchasedCourses;
 
-        await _studentsService.UpdateAsync(id, updatedStudent);
+        await _studentsService.UpdateAsync(id, student);
 
         return NoContent();
     }
+
+    //[HttpDelete("{id:length(24)}")]
+    //public async Task<IActionResult> Delete(string id)
+    //{
+    //    var student = await _studentsService.GetAsync(id);
+
+    //    if (student is null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    await _studentsService.RemoveAsync(id);
+
+    //    return NoContent();
+    //}
 
     [HttpDelete("{id:length(24)}")]
     public async Task<IActionResult> Delete(string id)
     {
-        var student = await _studentsService.GetAsync(id);
-
-        if (student is null)
+        try
         {
-            return NotFound();
+            var student = await _studentsService.GetAsync(id);
+
+            if (student is null)
+            {
+                return NotFound();
+            }
+
+            await _studentsService.RemoveAsync(id);
+
+            return NoContent();
         }
-
-        await _studentsService.RemoveAsync(id);
-
-        return NoContent();
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
 }
