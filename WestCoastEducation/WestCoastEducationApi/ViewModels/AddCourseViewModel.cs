a6bea9dabe71f2194dab1f3e2f78210b99b703ff
jova93 <89088512+jova93@users.noreply.github.com>
2022-03-31 10:26:21 +0200

    fifth commit

---
+++ WestCoastEducation/WestCoastEducationApi/ViewModels/AddCourseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+
+namespace WestCoastEducationApi.ViewModels;
+
+public class AddCourseViewModel
+{
+    public string? Id { get; set; }
+    public string Number { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public int Duration { get; set; }
+    public string Level { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public decimal Price { get; set; }
+
+    // This is the list of students that have bought this course.
+    public ICollection<ObjectId>? Participants { get; set; }
+}
---
+++ WestCoastEducation/WestCoastEducationApi/ViewModels/UpdateStudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+
+namespace WestCoastEducationApi.ViewModels;
+
+public class UpdateStudentViewModel
+{
+    public string? Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Address { get; set; } = null!;
+
+    // This keeps track on how much the student has spent on our company.
+    public decimal TotalSpent { get; set; }
+
+    // This is the courses that a student has bought.
+    public ICollection<ObjectId>? PurchasedCourses { get; set; }
+}
